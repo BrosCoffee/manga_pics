27fa0f6c13cdc8cef30d5d46fb0ade334d1b00ec
Raymond <raymondcyang0219@gmail.com>
2019-09-09 22:15:20 +0800

    manga bash script created

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
 import manga
 
-manga.get_pics(102,345)
+print('1. 航海王 (One Piece): 103')
+print('2. 火影忍者 (Naruto): 102')
+print('3. 一拳超人 (One Punch Man) 10406')
+
+print('請輸入漫畫代號(Please enter code)： ')
+manga_index = input()
+
+print('請輸入第幾話(Enter the chapter you like.)： ')
+chapter_index = input()
+
+manga.get_pics(manga_index,chapter_index)
