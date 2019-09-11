a2d15b06cb4242af60b69c55660711b606b2518c
Raymond <raymondcyang0219@gmail.com>
2019-09-11 10:58:55 +0800

    one_punch_man debug

--- pics_method.py
+++ pics_method.py
@@ -17,8 +17,23 @@ class Pics:
                
         html = driver.page_source  # 取得html文字
         # Find the pic and download it
-        pic_start_index = int(html.find('img3.8comic.com/2/'))
-        pic_end_index = pic_start_index + len('img3.8comic.com/2/') + len(str(manga_index)) + 1 + len(str(chapter_index)) + 12
+        # Determine which manga the user is interested, extendable.
+        # print(html)
+        def code(manga_index, chapter_index):
+            if manga_index == '103': # One Piece: 103
+                return 'img3.8comic.com/2/'
+            elif manga_index == '102': # Naruto: 102
+                return 'img3.8comic.com/2/'
+            elif manga_index == '10406': # One Punch Man: 10406            
+                if int(chapter_index) >= 157 or int(chapter_index) == 154: 
+                    return 'img3.8comic.com/3/'
+                elif int(chapter_index) >= 151 and int(chapter_index) <= 153:
+                    return 'img2.8comic.com/3/'
+                return 'img2.8comic.com/4/'
+
+        html_string = str(code(manga_index, chapter_index))
+        pic_start_index = int(html.find(html_string))
+        pic_end_index = pic_start_index + len(html_string) + len(str(manga_index)) + 1 + len(str(chapter_index)) + 12
         pic_url = 'http://' + html[pic_start_index:pic_end_index]
         print(pic_url)
       
